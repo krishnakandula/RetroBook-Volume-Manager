052ad662ee5f36ceedf9bccfc3d881ba98d6a718
Krishna Kandula <krishna.c.kandula@gmail.com>
2017-11-26 09:46:16 -0600

    Volume reader now return JSON object containing volume info

--- app/index.js
+++ app/index.js
@@ -5,11 +5,6 @@ const { reader } = require('./volumereader');
 
 const argv = yargs
         .command('create', 'Create a json file for a volume', {
-            path: {
-                describe: 'Parent folder file path',
-                demand: true,	
-                alias: 'p'
-            },
             volume: {
                 describe: 'Name of the volume',
                 demand: true,
@@ -23,9 +18,12 @@ const command = argv._[0];
 if (command) {
     switch(command) {
         case 'create':
-            reader(argv.path, argv.volume);
+            reader(argv.volume).then(volumeInfo => {
+                console.log(volumeInfo);
+            });
             break;
         default:
             console.error(`Command ${command} not found`);
     }
 }
+
--- app/volumereader.js
+++ app/volumereader.js
@@ -1,21 +1,52 @@
 const { AWS } = require('./config/awsconfig');
-let s3 = new AWS.S3();
+const fs = require('fs');
+const uuid = require('uuid/v1');
 
+let s3 = new AWS.S3();
 let params = {
     Bucket: "retrobooktest",
 };
 
-let reader = (path, volume) => {
-    params.Key = `${path}${volume}`;
-    s3.getObject(params).promise().then(file => {
-        JSON.parse(file).then(obj => {
-            console.log(obj);
-            
-        }, err => { console.error(err)});
-    }, err => {
-        console.error(err);
+const fileNames = {
+    root: 'volumes',
+    data: 'data.json',
+    cover: 'cover.jpg',
+    background: 'background.jpg'
+}
+
+let reader = (volume) => {
+    return new Promise((resolve, reject) => {
+        Promise.all([getVolumeData(`${fileNames.root}/${volume}`, fileNames.data)])
+            .then(info => {
+                let data = info[0];
+                resolve({
+                    id: uuid(),
+                    title: data.title,
+                    author: data.author,
+                    info: data.info,
+                    text: data.text
+                });
+            }).catch(err => {
+                console.error('Unable to retrieve info from S3', err);
+            });
     });
 };
+
+function getVolumeData(volume, dataFile) {
+    return new Promise((resolve, reject) => {
+        params.Key = `${volume}/${dataFile}`;
+        s3.getObject(params).promise().then(file => {
+            let data = file.Body.toString('utf8');
+            resolve(JSON.parse(data));
+        }, err => {
+            reject(err);
+        });
+    });
+}
+
+function getSignedImageUrl(path, volume, fileName) {
+
+}
 
 module.exports = {
     reader
